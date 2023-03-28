567604f1fddcda3be589e2a0fada150ff12eaa81
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-03-28 13:18:27 +0200

    petit modification

--- python/traduction.py
+++ python/traduction.py
@@ -5,7 +5,7 @@ class ArreraTrad :
     def __init__(self,langueInt,langueOut):
         self.traducteur = Translator(from_lang=langueInt,to_lang=langueOut)
         
-    def Tradution(text):
+    def Tradution(self,text):
         return self.traducteur.translate(str(text))
 
 
